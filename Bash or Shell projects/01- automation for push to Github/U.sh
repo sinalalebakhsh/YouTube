327bc804baabdc5575257481e23590d3fab1ae40
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-12-03 00:38:34 -0500

    add License MIT

---
+++ Bash or Shell projects/01- automation for push to Github/U.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+echo -n "Git push on branch develop "
+for i in {1..3}
+do
+    echo -n ". "
+    sleep 0.5
+done
+echo ""
+git add .
+git commit -m " ++ "
+git push -u origin develop
+echo "====================================================================="
+echo -n "git checkout beforeMergeToMain "
+for i in {1..3}
+do
+    echo -n ". "
+    sleep 0.5
+done
+echo ""
+git checkout beforeMergeToMain
+echo "====================================================================="
+echo -n "git merge develop "
+for i in {1..5}
+do
+    echo -n ". "
+    sleep 0.5
+done
+echo ""
+git merge develop
+echo "====================================================================="
+echo -n "Git is Push for beforeMergeToMain "
+for i in {1..3}
+do
+    echo -n ". "
+    sleep 0.5
+done
+echo ""
+git add .
+git commit -m " ++ "
+git push -u origin beforeMergeToMain
+echo "====================================================================="
+echo -n "git checkout develop "
+for i in {1..3}
+do
+    echo -n ". "
+    sleep 0.5
+done
+echo ""
+git checkout develop
+echo "====================================================================="
+
+
+
