637449fab077aa3c178ebf159654a4b5d3f8d9f8
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-02-06 06:34:16 -0500

    Go language projects and more 01- Hugo Installation

--- U.sh
+++ U.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m " 120- Hugo Book 
-Atishay Jain - Hugo in Action Static sites and dynamic Jamstack apps-Manning Publications (2022) "
+git commit -m " Go language projects and more
+01- Hugo Installation"
 git push -u origin main 
