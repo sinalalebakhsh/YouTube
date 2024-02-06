d282d87f0ff1df86c8c6c453ae225934ada9a77a
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-02-06 00:53:27 -0500

    120- Hugo Book Atishay Jain - Hugo in Action Static sites and dynamic Jamstack apps-Manning Publications (2022)

--- U.sh
+++ U.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m "119- Hugo Theory matters "
+git commit -m " 120- Hugo Book 
+Atishay Jain - Hugo in Action Static sites and dynamic Jamstack apps-Manning Publications (2022) "
 git push -u origin main 
