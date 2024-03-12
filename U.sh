3e1a7cf9c421542de3351eaf31cc812f824e75d1
sina lalehbakhsh <lalebakhshsina@gmail.com>
2024-03-12 03:24:08 -0400

    GO فارسی 126- Building a Basic Theme 1- in Terminal ➤➤➤➤ hugo new site portfolio Then ➤➤➤➤ cd portfolio 2- Create index.html in layouts directory ➤➤➤➤ index.html 3- copy/paste these code inside layouts/index.html 4- Create _index.md in Content directory 5- content in _index.md 6- in current directory write this  ➤➤➤➤ hugo server 7- in Terminal  ➤➤➤➤ hugo new about.md 8- in Terminal  ➤➤➤➤ mkdir layouts/_default/ 9- in Terminal  ➤➤➤➤ cp layouts/index.html layouts/_default/single.html 10- modify this line in content/about.md like this  ➤➤➤➤ draft = false 11- in Terminal ➤➤➤➤ hugo server 12- go in this link in browser  ➤➤➤➤ http://localhost:1313/about 13- Open the layouts/_default/single.html file in your editor and add the title like this: 14- go in this link in browser ➤➤➤➤ http://localhost:1313/about 15- in Terminal 16- Building and Exploring Hugo's Output ➤➤➤➤ in Terminal 17- delete the entire public folder ➤➤➤➤ in Terminal  ➤➤➤➤  rm -r public && hugo OR: use Hugo's --cleanDestinationDir argument ➤➤➤➤ in Terminal hugo --cleanDestinationDir 18- minify the files it generates ➤➤➤➤ in Terminal  ➤➤➤➤ hugo --cleanDestinationDir --minify 19- Create a 'Résumé' page 20- in Terminal ➤➤➤➤ hugo new theme basic 21- Move your existing layout files into this new theme ➤➤➤➤ in Terminal ➤➤➤➤ mv layouts/index.html themes/basic/layouts/index.html ➤➤➤➤ mv layouts/_default/single.html themes/basic/layouts/_default/single.html
    ➤➤➤➤

--- U.sh
+++ U.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 git add .
-git commit -m " Go language projects and more
-01- Hugo Installation"
+git commit -m " GO فارسی
+126- Building a Basic Theme
+1- in Terminal ➤➤➤➤ hugo new site portfolio
+Then ➤➤➤➤ cd portfolio
+2- Create index.html in layouts directory ➤➤➤➤ index.html
+3- copy/paste these code inside layouts/index.html
+4- Create _index.md in Content directory
+5- content in _index.md
+6- in current directory write this  ➤➤➤➤ hugo server
+7- in Terminal  ➤➤➤➤ hugo new about.md
+8- in Terminal  ➤➤➤➤ mkdir layouts/_default/
+9- in Terminal  ➤➤➤➤ cp layouts/index.html layouts/_default/single.html
+10- modify this line in content/about.md like this  ➤➤➤➤ draft = false
+11- in Terminal ➤➤➤➤ hugo server
+12- go in this link in browser  ➤➤➤➤ http://localhost:1313/about
+13- Open the layouts/_default/single.html file in your editor and add the title like this:
+14- go in this link in browser ➤➤➤➤ http://localhost:1313/about
+15- in Terminal
+16- Building and Exploring Hugo's Output ➤➤➤➤ in Terminal
+17- delete the entire public folder ➤➤➤➤ in Terminal  ➤➤➤➤  rm -r public && hugo
+OR:
+use Hugo's --cleanDestinationDir argument ➤➤➤➤ in Terminal
+hugo --cleanDestinationDir
+18- minify the files it generates ➤➤➤➤ in Terminal  ➤➤➤➤ hugo --cleanDestinationDir --minify
+19- Create a 'Résumé' page
+20- in Terminal ➤➤➤➤ hugo new theme basic
+21- Move your existing layout files into this new theme ➤➤➤➤ in Terminal 
+➤➤➤➤ mv layouts/index.html themes/basic/layouts/index.html
+➤➤➤➤ mv layouts/_default/single.html themes/basic/layouts/_default/single.html
+
+
+➤➤➤➤
+"
 git push -u origin main 
